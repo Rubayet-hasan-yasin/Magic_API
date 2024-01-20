4b74a49e614f63a3ef3435a21737c3051f5bdfe7
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2024-01-20 20:47:23 +0600

    get with DTO

---
+++ magic/Controllers/VillaController.cs
@@ -0,0 +1,23 @@
+﻿using magic.Models;
+using magic.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace magic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VillaController : ControllerBase
+    {
+
+        [HttpGet]
+        public IEnumerable<VillaDTO> GetVillas()
+        {
+            return new List<VillaDTO>
+            {
+                new VillaDTO {Id= 1, Name= "Pool View"},
+                new VillaDTO {Id= 2,Name= "Beach View"}
+            };
+        }
+    }
+}
