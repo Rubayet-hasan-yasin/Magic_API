59f481f6769864174a4f0af05c9e16613d505b0e
Rubayet-hasan-yasin <rubayethasanyasin@gmail.com>
2024-01-22 23:17:26 +0600

    update

--- magic/Controllers/VillaController.cs
+++ magic/Controllers/VillaController.cs
@@ -1,4 +1,5 @@
-﻿using magic.Models;
+﻿using magic.Data;
+using magic.Models;
 using magic.Models.DTO;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -11,13 +12,35 @@ namespace magic.Controllers
     {
 
         [HttpGet]
-        public IEnumerable<VillaDTO> GetVillas()
+        public ActionResult<IEnumerable<VillaDTO>> GetVillas()
         {
-            return new List<VillaDTO>
+            return Ok(VillaStore.villaList);
+        }
+
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]      //same 1
+        [ProducesResponseType(400)]
+        [ProducesResponseType(404)]
+        //[ProducesResponseType(typeof(VillaDTO), 200)]     //same 1
+        //[ProducesResponseType(400)]
+        //[ProducesResponseType(404)]
+
+        public ActionResult<VillaDTO?> GetVilla(int id)
+        {
+            if(id == 0)
             {
-                new VillaDTO {Id= 1, Name= "Pool View"},
-                new VillaDTO {Id= 2,Name= "Beach View"}
-            };
+                return BadRequest();
+            }
+
+            var villa = VillaStore.villaList.FirstOrDefault(x => x.Id == id);
+
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
         }
     }
 }
